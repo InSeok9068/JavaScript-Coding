3686e2df5b1e0f4ebb8de4df60f971b519b0049e
SANHAIT <dlstjr9068@naver.com>
2019-11-13 19:54:11 +0900

    반올림 처리

---
+++ math.js
@@ -0,0 +1,24 @@
+// const num = 99.6;
+
+// // 반올림
+// console.log(Math.round(num));
+
+// // 소수점 버리기
+// console.log(Math.floor(num));
+
+// // 소수점 올리기
+// console.log(Math.ceil(num));
+
+const num = 99.654;
+
+// 첫번째 자리까지 표시
+console.log(num.toFixed(1));
+// 99.7
+
+// 두번째 자리까지 표시
+console.log(num.toFixed(2));
+// 99.65
+
+// 세번째 자리까지 표시
+console.log(num.toFixed(3));
+// 99.654
